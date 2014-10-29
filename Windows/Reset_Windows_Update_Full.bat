65ada03f8bc67133a362663162326377382de4bf
Wei Wang <ww@9rivers.com>
2014-10-29 16:00:54 -0400

    Updared with script from Reddit.

--- Windows/Reset_Windows_Update_Full.bat
+++ Windows/Reset_Windows_Update_Full.bat
@@ -1,14 +1,31 @@
 :: Created by: Shawn Brink
 :: http://www.sevenforums.com
 :: Tutorial: http://www.sevenforums.com/tutorials/91738-windows-update-reset.html
+::
+:: 2014-10-29 Updated by: Wei Wang
+:: http://www.reddit.com/r/usefulscripts/comments/2e5kte/batch_reset_windows_update/
 
 
 net stop bits
 net stop wuauserv
 Del "%ALLUSERSPROFILE%\Application Data\Microsoft\Network\Downloader\qmgr*.dat"
-rmdir %windir%\softwaredistribution /s /q
+
+Rmdir "%Windir%\SoftwareDistribution\Download" /s /q 
+Rmdir "%systemroot%\SoftwareDistribution\DataStore" /s /q 
+Rmdir "%systemroot%\system32\catroot2" /s /q
+::: Windows XP & 2003 - Delete %systemroot%\SoftwareDistribution\ as well
+ver | findstr /i "5\.1\." > nul
+IF %ERRORLEVEL% EQU 0 goto XP_OR_2003
+ver | findstr /i "5\.2\." > nul
+IF %ERRORLEVEL% EQU 0 goto XP_OR_2003
+GOTO CONTINUE
+:XP_OR_2003
+Rmdir "%Windir%\SoftwareDistribution" /s /q 
+:CONTINUE
+
 sc.exe sdset bits D:(A;;CCLCSWRPWPDTLOCRRC;;;SY)(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;BA)(A;;CCLCSWLOCRRC;;;AU)(A;;CCLCSWRPWPDTLOCRRC;;;PU)
 sc.exe sdset wuauserv D:(A;;CCLCSWRPWPDTLOCRRC;;;SY)(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;BA)(A;;CCLCSWLOCRRC;;;AU)(A;;CCLCSWRPWPDTLOCRRC;;;PU)
+
 cd /d %windir%\system32
 regsvr32.exe /s atl.dll
 regsvr32.exe /s urlmon.dll
@@ -51,10 +68,5 @@ netsh winsock reset
 net start bits
 net start wuauserv
 bitsadmin.exe /reset /allusers
+wuauclt /resetauthorization /detectnow
 shutdown /r
-
-
-
-
-
-
